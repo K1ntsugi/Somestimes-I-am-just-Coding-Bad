6050d9dd2764d8295c04280458e8b5d023fbd2d8
escapedraccoon <24590998+escapedraccoon@users.noreply.github.com>
2022-07-10 13:27:17 +0200

    mhm

--- ap1/ap1Class.java
+++ ap1/ap1Class.java
@@ -1,6 +1,5 @@
 public class ap1Class {
 
-
     /* Merge Two
     Start with two arrays of strings, A and B, each with its elements
     in alphabetical order and without duplicates.
@@ -17,7 +16,6 @@ public class ap1Class {
     mergeTwo(["a", "c", "z"], ["c", "f", "z"], 3) → ["a", "c", "f"]
     mergeTwo(["f", "g", "z"], ["c", "f", "g"], 3) → ["c", "f", "g"]
  */
-
 
     public String[] mergeTwo(String[] a, String[] b, int n) {
   
@@ -50,6 +48,16 @@ public class ap1Class {
 
     //@TODO: CommonTwo fix
 
+    /*
+     * Start with two arrays of strings, a and b, each in alphabetical order, possibly with duplicates.
+     * Return the count of the number of strings which appear in both arrays. The best "linear" solution makes
+     * a single pass over both arrays, taking advantage of the fact that they are in alphabetical order.
+     * 
+     * commonTwo(["a", "c", "x"], ["b", "c", "d", "x"]) → 2
+     * commonTwo(["a", "c", "x"], ["a", "b", "c", "x", "z"]) → 3
+     * commonTwo(["a", "b", "c"], ["a", "b", "c"]) → 3
+     */
+
     public int commonTwo(String[] a, String[] b) {
         int j = 0;
         int k = 0;
@@ -74,6 +82,25 @@ public class ap1Class {
         }
         return count;
     }
+
+
+    /* 
+     * 
+     * Given two arrays, A and B, of non-negative int scores. 
+     * A "special" score is one which is a multiple of 10, such as 40 or 90.
+     * Return the sum of largest special score in A and the largest special score in B.
+     * To practice decomposition, write a separate helper method which finds the largest
+     * special score in an array. Write your helper method after your scoresSpecial()
+     * method in the JavaBat text area.
+     * scoresSpecial([12, 10, 4], [2, 20, 30]) → 40
+     * scoresSpecial([20, 10, 4], [2, 20, 10]) → 40
+     * scoresSpecial([12, 11, 4], [2, 20, 31]) → 20
+     * 
+     */
+
+    public int scoresSpecial(int[] a, int[] b) {
+        
+    }    
 
 
 }
