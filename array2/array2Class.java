c27cfda0909ec81f6d65345af6fb2fccdfecab43
escapedraccoon <24590998+escapedraccoon@users.noreply.github.com>
2022-07-12 10:20:00 +0200

    aha...???

--- array2/array2Class.java
+++ array2/array2Class.java
@@ -49,6 +49,13 @@ public class array2Class {
         }
         return returnArr;
       }
+
+      public boolean only14(int[] nums) {
+        for(int i = 0; i < nums.length; i++) {
+          if(nums[i] != 4 && nums[i] != 1) return false;
+        }
+        return true;
+      }
       
 
 }
